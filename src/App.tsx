0bcd5d89f364a6833471274597b6a63f1569e68f
David Lopes <davidlopes009@gmail.com>
2022-06-22 11:38:05 -0300

    Ignite Lab - Day 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,15 @@
-import { gql, useQuery } from "@apollo/client"
-import { useEffect } from "react"
-import { client } from "./lib/apollo"
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-
-    }
-  }
-`
-interface lesson {
-  id: string;
-  title: string;
-}
+import { Header } from "./components/header"
+import { Lesson } from "./components/lesson"
+import { Sidebar } from "./components/sidebar"
+import { Video } from "./components/video"
+import { Event } from "./pages/Event"
 
 function App() {
 
-  const {data} = useQuery<{lessons: lesson[]}>(GET_LESSONS_QUERY)
-
   return (
-    <ul>
-      {data?.lessons.map(lesson => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
+    <div>
+      <Event />
+    </div>
   )
 }
 
